944448c4ad62c7816f230b9fb75f6e7f66173a8c
Takuya Horimatsu <new_woory@hotmail.com>
2017-08-30 01:16:57 +0900

    supporting date

--- src/main/kotlin/read1.kt
+++ src/main/kotlin/read1.kt
@@ -1,4 +1,5 @@
 import org.apache.poi.ss.usermodel.Cell
+import org.apache.poi.ss.usermodel.DateUtil
 import org.apache.poi.ss.usermodel.Row
 import org.apache.poi.ss.usermodel.WorkbookFactory
 import java.io.BufferedWriter
@@ -6,9 +7,17 @@ import java.io.File
 import java.nio.charset.Charset
 import java.nio.file.Files
 import java.nio.file.Paths
+import java.time.ZoneId
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+
 
 
 fun main(args: Array<String>) {
+  val dateFormat = DateTimeFormatter.ofPattern("yyyy/MM/dd")
+  val dateTimeFormat = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss.SSS")
+
   val csvEncode = Charset.forName("Shift_JIS")
   var sheetList = listOf("東京")
 
@@ -30,7 +39,16 @@ fun main(args: Array<String>) {
 
           println("$i, $j : " + cell.cellType)
           when (cell.cellType) {
-            Cell.CELL_TYPE_NUMERIC -> lineCells.add(cell.numericCellValue.toString())
+            Cell.CELL_TYPE_NUMERIC -> {
+              if (DateUtil.isCellDateFormatted(cell)) {
+                val date = cell.dateCellValue
+
+                val localDateTime = LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault())
+                lineCells.add(dateFormat.format(localDateTime))
+              } else {
+                lineCells.add(cell.numericCellValue.toString())
+              }
+            }
             Cell.CELL_TYPE_STRING -> lineCells.add(cell.stringCellValue)
             Cell.CELL_TYPE_FORMULA -> {
               println(String.format("Formula:=%s cachedType:%d %s",
