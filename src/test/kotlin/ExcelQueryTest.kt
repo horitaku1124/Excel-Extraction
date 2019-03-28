609807fa29e85cf9029bb6002c0a3cad786a3e5f
Horimatsu Takuya <new_woory@hotmail.com>
2019-03-28 22:12:12 +0900

    not use mutli byte char

--- src/test/kotlin/ExcelQueryTest.kt
+++ src/test/kotlin/ExcelQueryTest.kt
@@ -24,9 +24,9 @@ class ExcelQueryTest {
   }
   @Test
   fun test1() {
-    ExcelQuery.main(arrayOf("select 日付 from `./data/sample1.xlsx`.東京"))
+    ExcelQuery.main(arrayOf("select A from `./data/sample1.xlsx`.Tokyo"))
 
     var result = outContent.toString().replace("\r\n", "\n")
-    assertThat(result, Is("日付\n2017/01/01\n2017/10/11\n\n"))
+    assertThat(result, Is("A\n1\n2\n3\n"))
   }
 }
